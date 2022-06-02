3ebda773a2df67427958d8ea5d7a3702ef10d745
shaqdeff <shaquillendunda@gmail.com>
2022-06-03 01:29:05 +0300

    Install and setip HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js'
