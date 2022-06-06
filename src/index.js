1e3afd96be116e112812d33633e2bb030a3027ea
shaqdeff <shaquillendunda@gmail.com>
2022-06-06 22:26:09 +0300

    Implement add and remove function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import './style.css';
 
 // Array to hold todo list items
-const todoItems = [];
+let todoItems = [];
 let index = 0;
 
 const renderTodo = (todo) => {
   // select ul element
   const list = document.querySelector('.todo-list');
+  // select current item in the DOM
+  const item = document.querySelector(`[data-key='${todo.index}']`);
+
+  // remove item from DOM
+  if (todo.deleted) {
+    item.remove();
+    return;
+  }
 
   // check if todo is completed
   const isCompleted = todo.completed ? 'done' : '';
@@ -17,13 +25,18 @@ const renderTodo = (todo) => {
   li.setAttribute('data-key', todo.index);
 
   li.innerHTML = `
-    <input id="${todo.index}" type="checkbox"/>
-    <label for="${todo.index}" class="tick js-tick"></label>
-    <span>${todo.description}</span>
-    <i class="fa fa-trash-alt delete"></i>
+  <input id="${todo.index}" type="checkbox"/>
+  <label for="${todo.index}" class="tick js-tick"></label>
+  <span class="todo-desc">${todo.description}</span>
+  <i class="fa fa-edit edit"></i>
+  <i class="fa fa-trash-alt delete"></i>
   `;
 
-  list.append(li);
+  if (item) {
+    list.replaceChild(li, item);
+  } else {
+    list.append(li);
+  }
 };
 
 // Function to create new todo object
@@ -37,7 +50,27 @@ const addTodo = (description) => {
   index += 1;
   todoItems.push(todo);
   renderTodo(todo);
-  console.log(todoItems);
+};
+
+const toggleDone = (key) => {
+  const index = todoItems.findIndex((item) => item.index === Number(key));
+  todoItems[index].completed = !todoItems[index].completed;
+  renderTodo(todoItems[index]);
+};
+
+const deleteTodo = (key) => {
+  // find item in the todo items array
+  const index = todoItems.findIndex((item) => item.index === Number(key));
+
+  // create new object with a deleted property
+  const todo = {
+    deleted: true,
+    ...todoItems[index],
+  };
+
+  // remove todo item from array
+  todoItems = todoItems.filter((item) => item.index !== Number(key));
+  renderTodo(todo);
 };
 
 // select form
@@ -72,13 +105,22 @@ plusIcon.addEventListener('click', () => {
   }
 });
 
-// delete todo items
+
 // select entire list
 const list = document.querySelector('.todo-list');
 
-// add click event listener to trash icon
-list.addEventListener('click', e => {
-  if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove()
+// Add a click event listener to the list and its children
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('js-tick')) {
+    const itemKey = e.target.parentElement.dataset.key;
+    toggleDone(itemKey);
   }
-})
+});
+
+// add click event listener to trash icon
+list.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+    const itemKey = e.target.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
